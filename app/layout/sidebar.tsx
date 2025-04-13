7ce8a5b3b16c2820ed67e78dfddfff2e59f0d620
bikash119 <bikashjpatra@gmail.com>
2025-04-13 19:34:57 +0530

    remove button from list of contacts in sidebar

--- app/layout/sidebar.tsx
+++ app/layout/sidebar.tsx
@@ -24,10 +24,10 @@ export default function Sidebar({loaderData}: Route.ComponentProps){
                 <div>{contact.first || contact.last ? `${contact.first} ${contact.last}` : contact.company}</div>
                 <div className="text-xs font-semibold opacity-60">{contact.notes}</div>
                 </div>
-                <button className="btn btn-square btn-ghost">
-                    <svg className="size-[1.2em]"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill={contact.favorite ? "bg-indigo-500" : "none"} stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                </button>
+
+                <svg className="size-[1.2em]"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill={contact.favorite ? "bg-indigo-500" : "none"} stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
+
             </li>
         </NavLink>
     ));
